a5d146635ec8be552198967cfb5dd586ec48589e
Derek Klass <d.klass@student.boyertownasd.org>
2020-01-06 10:20:13 -0500

    boxscore

--- test.py
+++ test.py
@@ -1,14 +1,29 @@
 from sportsreference.nba.teams import Teams
 from sportsreference.nba.boxscore import Boxscore
 from sportsreference.nba.boxscore import Boxscores
+from sportsreference.nba.schedule import Schedule
 from datetime import datetime
 
 teams = Teams()
 print('\nnew run\n')
 
+'''
 for team in teams:
     print(team.name)
     print(team.games_played)
+'''
 
-games_today = Boxscores(datetime(2020, 1, 5))
-print(games_today.games)
+specificgame = Boxscore('202001050LAC')
+print("\n" + str(specificgame.home_blocks))
+
+phila_schedule = Schedule('PHI')
+
+for i in phila_schedule:
+    if i.date == "Tue, Nov 12, 2019":
+        print(i.boxscore_index)
+        print(i.dataframe)
+
+game2018 = Boxscore('201810160BOS')
+
+print(game2018.winner)
+
