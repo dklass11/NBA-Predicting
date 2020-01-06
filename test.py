7f48715855f79c180354299ae4c11a1336370f1b
Derek Klass <d.klass@student.boyertownasd.org>
2020-01-06 09:16:10 -0500

    gfsr

--- test.py
+++ test.py
@@ -1,7 +1,14 @@
 from sportsreference.nba.teams import Teams
+from sportsreference.nba.boxscore import Boxscore
+from sportsreference.nba.boxscore import Boxscores
+from datetime import datetime
 
 teams = Teams()
+print('\nnew run\n')
 
 for team in teams:
     print(team.name)
     print(team.games_played)
+
+games_today = Boxscores(datetime.today())
+print(games_today.games)
